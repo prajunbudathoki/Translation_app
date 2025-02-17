0d9d3fc4b338d749d978c8f08d7acd782a27cc3c
Prajun <prajunbudathoki1@gmail.com>
2025-02-17 11:07:26 +0545

    worked for the transcribing audio and laoded the skeleton effect

--- Translation_app/src/App.jsx
+++ Translation_app/src/App.jsx
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from 'react'
 import HomePage from './components/HomePage';
 import Header from './components/Header';
 import FileDisplay from './components/FileDisplay';
+import Information from './components/Information';
+import Transcribe from './components/Transcribe';
 
 const App = () => {
   const [file, setFile] = useState(null)
   const [audioStream,setAudioStream] = useState(null)
+  const [output,setOutput] = useState(null)
+  const [loading,setLoading] = useState(true)
 
   const isAudioAvailable = file || audioStream
 
@@ -22,9 +26,19 @@ const App = () => {
     <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
       <section className='min-h-screen flex flex-col'>
         <Header />
-        {isAudioAvailable ? 
+        {output ? (
+          <Information/>
+          ) : loading ? (
+          <Transcribe />
+          ) : isAudioAvailable ? (
+          <FileDisplay handleAudio={handleAudio}
+           file={file} audioStream={setAudioStream} />
+           ) : (
+           <HomePage setFile={setFile} setAudioStream={setAudioStream} /> 
+           )}
+        {/* {isAudioAvailable ? 
         (<FileDisplay file={file} setAudioStream={setAudioStream} handleAudio={handleAudio} />) : 
-        (<HomePage setFile={setFile} setAudioStream={setAudioStream} />)}
+        (<HomePage setFile={setFile} setAudioStream={setAudioStream} />)} */}
       </section>
       <h1 className='text-green-400'>App</h1>
       <footer>
--- Translation_app/src/components/Transcribe.jsx
+++ Translation_app/src/components/Transcribe.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-const Transcribe = () => {
+const Transcribe = (props) => {
+  const {downloading} = props
   return (
-    <div className="flex items-center flex-col justify-center gap-10 md:gap-14 py-24">
-        <div className="flex flex-col gap-2 "></div>
+    <div className="flex items-center flex-1 flex-col justify-center gap-10 md:gap-14 pb-24 text-center">
+        <div className="flex flex-col gap-4 ">
+          <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl'>
+            <span className='text-blue-300'>Transcribing</span>
+          </h1>
+          <p>{!downloading ? 'loading....🫸' : 'Engaged complete 👍'}</p>
+        </div>
+        <div className="flex flex-col gap-2 sm:gap-4 max-w-[500px] mx-auto w-full">
+          {[0,1,2].map(val => {
+            return(
+              <div key={val} className={'rounded-lg h-2 sm:h-3 bg-slate-300 loading' + `loading ${val}`}></div>
+            )
+          })}
+        </div>
     </div>
   )
 }
